3e45b732f0fc3e1fa68fbfcc65cfb1b4dd5924ac
puentedg <puentedg@gmail.com>
2022-11-16 19:16:04 -0500

    thought and user controllers done

---
+++ controllers/thought.js
@@ -0,0 +1,91 @@
+const {Thought, User} = require('../models');
+const {Types} = require('mongoose');
+
+const notFound = "No thought with that ID";
+
+module.exports = {
+    async getThoughts(req, res) {
+        Thought.find()
+        .then((thought) => res.json(thought))
+        .catch((err) => res.status(500).json(err));
+    },
+
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const updatedUser = await User.findOneAndUpdate(
+                {_id: Types.ObjectId(req.body.userId)},
+                {$addToSet: {thoughts: Types.ObjectId(thought._id)}},
+                {runValidators: true, new: true}
+            );
+            updatedUser ? res.json(updatedUser) : res.status(404).json({message: notFound});
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    },
+
+    async getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .then((thought) =>
+          !thought
+            ? res.status(404).json({ message: 'No tought with that ID' })
+            : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
+    async deleteThought(req, res) {
+        try {
+            const deletedThought = await Thought.findOneAndDelete({_id: Types.ObjectId(req.params.thoughtId)});
+            await User.findOneAndUpdate(
+                {thoughts: Types.ObjectId(deletedThought._id)},
+                {$pull: {thoughts: Types.ObjectId(deletedThought._id)}},
+                {runValidators: true, new: true}
+            );
+            deletedThought ? res.json(deletedThought) : res.status(404).json({message: notFound});
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    },
+
+    async updateThought(req, res) {
+        try {
+            const updatedThought = await Thought.findOneAndUpdate(
+                {_id: Types.ObjectId(req.params.thoughtId)},
+                {$set: req.body},
+                {runValidators: true, new: true}
+            );
+            updatedThought ? res.json(updatedThought) : res.status(404).json({message: notFound});
+
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    },
+
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: Types.ObjectId(req.params.thoughtId)},
+                {$addToSet: {reactions: req.body}},
+                {runValidators: true, new: true}
+            );
+            thought ? res.json(thought) : res.status(404).json({message: notFound});
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    },
+
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                {_id: Types.ObjectId(req.params.thoughtId)},
+                {$pull: {reactions: {_id: Types.ObjectId(req.params.reactionId)}}},
+                {runValidators: true, new: true}
+            );
+
+            thought ? res.json(thought) : res.status(404).json({message: notFound});
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    }
+}
